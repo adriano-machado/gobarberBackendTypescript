ebc209b1c5ce497cfe945450950ae42cc6da918a
Adriano Machado <adrianoricardom@gmail.com>
2020-05-17 17:46:27 -0300

    changing imports

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -2,11 +2,10 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
-
+import '@shared/infra/typeorm';
+import uploadConfig from '@config/upload';
+import AppError from '@shared/errors/AppError';
 import routes from './routes';
-import './database';
-import uploadConfig from './config/upload';
-import AppError from './errors/AppError';
 
 const app = express();
 app.use(cors());
