d7b27f9135e6b9fdf38e0e90b06983efadedc18e
Adriano Machado <adrianoricardom@gmail.com>
2020-05-22 07:58:32 -0300

    creating test to sent email

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -2,6 +2,9 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskstorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import IMailProvider from './MailProvider/models/IMailProvider';
+// import MailProvider from './MailProvider/implementations/MailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskstorageProvider,
