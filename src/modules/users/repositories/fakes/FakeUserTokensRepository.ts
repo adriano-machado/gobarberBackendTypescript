c28c25a5e139145e755beb7f36a29d2373daa408
Adriano Machado <adrianoricardom@gmail.com>
2020-05-22 11:46:50 -0300

    more tests

--- src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
+++ src/modules/users/repositories/fakes/FakeUserTokensRepository.ts
@@ -11,6 +11,13 @@ class FakeUserTokensRepository implements IUserTokensRepository {
     this.userTokens.push(userToken);
     return userToken;
   }
+
+  public async findyByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = this.userTokens.find(
+      findToken => findToken.token === token,
+    );
+    return userToken;
+  }
 }
 
 export default FakeUserTokensRepository;
