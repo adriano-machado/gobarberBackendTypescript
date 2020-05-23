0405a4ac560958e0875b81bc96582d3f133bfcbf
Adriano Machado <adrianoricardom@gmail.com>
2020-05-23 10:53:31 -0300

    create:routes , controller and migration to userToken

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -18,6 +18,7 @@ class SendForgotPasswordEmailService {
 
     @inject('MailProvider')
     private mailProvider: IMailProvider,
+
     @inject('UserTokensRepository')
     private userTokensRepository: IUserTokensRepository,
   ) {}
