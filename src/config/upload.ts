626e081413d78ae38d1d249396e9bc5fcdba7e71
Adriano Machado <adrianoricardom@gmail.com>
2020-05-28 07:45:33 -0300

    config aws S3 - bucket storage

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -1,17 +1,38 @@
 import path from 'path';
-import multer from 'multer';
+import multer, { StorageEngine } from 'multer';
 import crypto from 'crypto';
 
+interface IUploadConfig {
+  driver: 's3' | 'disk';
+  tmpFolder: string;
+  uploadsFolder: string;
+  multer: {
+    storage: StorageEngine;
+  };
+  config: {
+    disk: {};
+    aws: {
+      bucket: string;
+    };
+  };
+}
 const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 export default {
+  driver: process.env.STORAGE_DRIVER,
   tmpFolder,
   uploadsFolder: path.resolve(tmpFolder, 'uploads'),
-  storage: multer.diskStorage({
-    destination: tmpFolder,
-    filename: (req, file, cb) => {
-      const fileHash = crypto.randomBytes(10).toString('HEX');
-      const fileName = `${fileHash}-${file.originalname}`;
-      return cb(null, fileName);
-    },
-  }),
-};
+  multer: {
+    storage: multer.diskStorage({
+      destination: tmpFolder,
+      filename: (req, file, cb) => {
+        const fileHash = crypto.randomBytes(10).toString('HEX');
+        const fileName = `${fileHash}-${file.originalname}`;
+        return cb(null, fileName);
+      },
+    }),
+  },
+  config: {
+    aws: { bucket: 'app-gobarber-bucket-teste2' },
+    disk: {},
+  },
+} as IUploadConfig;
